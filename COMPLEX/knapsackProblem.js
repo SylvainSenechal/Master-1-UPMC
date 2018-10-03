7832a4aa3f2b2af3aab85419c60e8eaf953fbbdf
Sylvain Sénéchal <senechalsylvain@hotmail.com>
2018-10-03 21:46:26 +0200

    added benchmarks & chart

--- COMPLEX/knapsackProblem.js
+++ COMPLEX/knapsackProblem.js
@@ -1,33 +1,57 @@
 // NB : voir si poids de 0 et utilite de 0 possibles (oui pour pas diviser par zero)
 listObjects = []
-listObjectsSize = 50000
-
-rangeWeight = 100
-rangeUtility = 10
+listObjectsREFERENCEsize = 1000000
+listObjectsSize = null
+Rparameter = 100
 
 backpack = []
-maxBackpackWeight = 100000
+backpackCapacity = null
 
+const calculateBackpackCapacity = () =>{
 
-const initList = () =>{
-  startingTime = performance.now()
-
-  for(let i=0; i<listObjectsSize; i++){
-    listObjects[i] = {poids: Math.floor(Math.random()*rangeWeight)+1, utilite: Math.floor(Math.random()*rangeUtility)+1}
-    listObjects[i].benefice = listObjects[i].utilite / listObjects[i].poids
-  }
-  console.log(listObjects)
-  listObjects.sort( (object1, object2) => object2.benefice - object1.benefice )
-  console.log(listObjects)
-
-  fillBackPack()
+  let capacity = 0
+  listObjects.forEach( object => capacity += object.poids)
+  backpackCapacity = Math.floor(capacity/2)
 }
 
 var startingTime, endTime
-const fillBackPack = () =>{
-  let cpt = 0
-  let weightLeft = maxBackpackWeight
+var context
+var myChart
+var dataChart = [], dataLabels = []
 
+const init = () =>{
+  createChart()
+
+  for(let nbIteration=0; nbIteration<30; nbIteration++){ // Double boucle, 1 pour R, 1 pour Tmax
+    startingTime = performance.now()
+    createBackpack(nbIteration)
+    calculateBackpackCapacity()
+    fillBackPack()
+    endTime = performance.now()
+    calculationTime = endTime - startingTime
+    console.log("calculationTime : " + calculationTime) // NB : c est le tri qui est tres long
+
+    dataChart.push(calculationTime)
+    dataLabels.push(nbIteration)
+    myChart.data.labels = dataLabels
+    myChart.data.datasets[0].data = dataChart
+    myChart.update()
+  }
+}
+
+const createBackpack = (nbIteration) =>{
+  listObjectsSize = ((nbIteration+1) * listObjectsREFERENCEsize) / 10
+  for(let i=0; i<listObjectsSize; i++){
+    listObjects[i] = {poids: Math.floor(Math.random()*Rparameter)+1, utilite: Math.floor(Math.random()*Rparameter)+1}
+    listObjects[i].benefice = listObjects[i].utilite / listObjects[i].poids
+  }
+  listObjects.sort( (object1, object2) => object2.benefice - object1.benefice )
+}
+
+const fillBackPack = () =>{
+  backpack = [] // Vider le backpack pour nouvelle iterations
+  let cpt = 0
+  let weightLeft = backpackCapacity
   while(weightLeft > 0 && cpt < listObjectsSize){
     if(weightLeft >= listObjects[cpt].poids){
       backpack.push(listObjects[cpt])
@@ -35,23 +59,31 @@ const fillBackPack = () =>{
     }
     cpt++
   }
-  endTime = performance.now()
 
-  calculationTime = endTime - startingTime
-  console.log(backpack)
-  console.log("Weight left : " + weightLeft)
-  console.log("calculationTime : " + calculationTime) // NB : c est le tri qui est tres long
+  // console.log(backpack)
+  // console.log("Weight left : " + weightLeft)
 }
 
-console.log(listObjectsSize.length)
 
+function createChart(){
+  context = document.getElementById("myChart").getContext('2d');
+  myChart = new Chart(context, {
+    type: 'line',
+    data: {
+      datasets: [{
+        label: 'Script time',
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    }
+  });
+}
 
-
-
-
-
-
-
-
-
-window.addEventListener('load', initList); // La fonction est lancee des que la page est prete
+window.addEventListener('load', init); // La fonction est lancee des que la page est prete
